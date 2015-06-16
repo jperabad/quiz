74b0b7dae03d9fd508ee0d8f0afbb0cf8ce804f2
jperabad <jperabad@Mac-mini-de-Jordi.local>
2015-06-16 16:27:38 +0200

    Crear comentario

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,14 @@ var models = require('../models/models.js');
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.find(quizId).then(
+  models.Quiz.find({
+            where: {
+                id: Number(quizId)
+            },
+            include: [{
+                model: models.Comment
+            }]
+        }).then(
     function(quiz) {
       if (quiz) {
         req.quiz = quiz;
@@ -67,7 +74,7 @@ exports.answer = function(req, res) {
 // GET /quizes/new
 exports.new = function(req, res) {
   var quiz = models.Quiz.build(
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    {pregunta: "", respuesta: ""}
   );
 
   res.render('quizes/new', {quiz: quiz, errors: []});
